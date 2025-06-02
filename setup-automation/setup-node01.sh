3958903d02146b22c3fdf382809dc5a1ff9dcaad
Nuno Martins <nmartins@redhat.com>
2025-06-02 11:16:08 +0200

    Updated host scripts

---
+++ setup-automation/setup-node01.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
+do
+    echo "Waiting for Instruqt to finish booting the VM"
+    sleep 1
+done
+
+touch /etc/sudoers.d/rhel_sudoers
+echo "%rhel ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/rhel_sudoers
+cp -a /root/.ssh/* /home/rhel/.ssh/.
+chown -R rhel:rhel /home/rhel/.ssh
+
+## clean repo metadata and refresh
+dnf config-manager --disable google*
+dnf clean all
+dnf config-manager --enable rhui-rhel-9-for-x86_64-baseos-rhui-rpms
+dnf config-manager --enable rhui-rhel-9-for-x86_64-appstream-rhui-rpms
+dnf makecache
+setenforce 0
+
+dnf install httpd nano -y
+
+
+
+cat <<EOF | tee /var/www/html/index.html
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nothing to See Here</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f9;
+            color: #333;
+        }
+        h1 {
+            font-size: 3em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Nothing to See Here - Not Yet Anyway - Node01 </h1>
+</body>
+</html>
+
+EOF
+
+systemctl start httpd
+
+mkdir /backup
+chmod -R 777 /backup
