2485f2bcbacfb590906de4c71456c317970c2f42
Alberto Gonzalez Rodriguez <josegonz@redhat.com>
2025-06-02 15:08:34 +0200

    Update setup-node03.sh

--- setup-automation/setup-node03.sh
+++ setup-automation/setup-node03.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
-do
-    echo "Waiting for Instruqt to finish booting the VM"
-    sleep 1
-done
 
-touch /etc/sudoers.d/rhel_sudoers
-echo "%rhel ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/rhel_sudoers
-cp -a /root/.ssh/* /home/rhel/.ssh/.
-chown -R rhel:rhel /home/rhel/.ssh
-
-## clean repo metadata and refresh
-dnf config-manager --disable google*
-dnf clean all
-dnf config-manager --enable rhui-rhel-9-for-x86_64-baseos-rhui-rpms
-dnf config-manager --enable rhui-rhel-9-for-x86_64-appstream-rhui-rpms
-dnf makecache
 setenforce 0
 
 dnf install httpd nano -y
