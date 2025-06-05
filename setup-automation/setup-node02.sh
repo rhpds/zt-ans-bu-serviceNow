6f04c15bfe0511c9ebbd4dc8c48a26af6708cd04
Alberto Gonzalez Rodriguez <josegonz@redhat.com>
2025-06-05 08:32:22 +0200

    Update setup-node02.sh

--- setup-automation/setup-node02.sh
+++ setup-automation/setup-node02.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
+curl -k  -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
 update-ca-trust
 rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm
 
