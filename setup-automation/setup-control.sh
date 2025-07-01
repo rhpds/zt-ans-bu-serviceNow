8ffa22d961f6ae4141496e1a25ea5f3c05f6f436
Alex Walczyk <abwalczyk@gmail.com>
2025-07-01 12:09:01 -0400

    dont disable tmpfile setup

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-systemctl stop systemd-tmpfiles-setup.service
-systemctl disable systemd-tmpfiles-setup.service
+#systemctl stop systemd-tmpfiles-setup.service
+#systemctl disable systemd-tmpfiles-setup.service
 
 # Install collection(s)
 ansible-galaxy collection install community.general
