ec8d824b005d4fe41be752cffee3ab5fde5575c4
Alex Walczyk <abwalczyk@gmail.com>
2025-07-14 19:05:18 -0400

    Changing from awx.awx to ansible.controller collection

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 USER=rhel
-# Install required AWX collection
-su - rhel -c 'ansible-galaxy collection install awx.awx'
-
-# Create necessary project directories
-su - rhel -c 'mkdir /tmp/admin_project/'
-chmod a+x /tmp/admin_project/
-su - rhel -c 'mkdir /home/rhel/servicenow_project'
-chmod a+x /home/rhel/
-su - awx -c 'ln -s /home/rhel/servicenow_project/ /var/lib/awx/projects/'
 
 # Create an inventory file for this environment
 tee /tmp/inventory << EOF
 [nodes]
 node01
 node02
-
-[storage]
-storage01
 
 [all]
 node01
@@ -36,7 +24,7 @@ sudo chown rhel:rhel /tmp/inventory
 
 
 # Controller configuration playbook (local only)
-tee /home/rhel/setup-controller.yml << EOF
+tee /tmp/setup-controller.yml << EOF
 ---
 - name: Configure learner Controller 
   hosts: localhost
@@ -114,7 +102,7 @@ tee /home/rhel/setup-controller.yml << EOF
           SN_HOST: https://ansible.service-now.com
 
     - name: add rhel machine credential
-      awx.awx.credential:
+      ansible.controller.credential:
         name: 'rhel credential'
         organization: Default
         credential_type: Machine
@@ -127,7 +115,7 @@ tee /home/rhel/setup-controller.yml << EOF
           password: ansible123!
 
     - name: add rhel inventory
-      awx.awx.inventory:
+      ansible.controller.inventory:
         name: "rhel inventory"
         description: "rhel servers in demo environment"
         organization: "Default"
@@ -138,7 +126,7 @@ tee /home/rhel/setup-controller.yml << EOF
         validate_certs: false
 
     - name: add hosts
-      awx.awx.host:
+      ansible.controller.host:
         name: "{{ item }}"
         description: "rhel host"
         inventory: "rhel inventory"
@@ -153,7 +141,7 @@ tee /home/rhel/setup-controller.yml << EOF
         - node2
 
     - name: Add group
-      awx.awx.group:
+      ansible.controller.group:
         name: nodes
         description: "rhel host group"
         inventory: rhel inventory
@@ -167,107 +155,12 @@ tee /home/rhel/setup-controller.yml << EOF
         controller_password: ansible123!
         validate_certs: false
 
-    - name: Add student project
-      awx.awx.project:
-        name: "ServiceNow"
-        description: "Project containing users ServiceNow playbooks"
-        organization: Default
-        state: present
-        scm_type: git
-        scm_url: https://github.com/cloin/instruqt-snow
-        controller_host: "https://localhost"
-        controller_username: admin
-        controller_password: ansible123!
-        validate_certs: false
-
-    - name: Add admin project
-      awx.awx.project:
-        name: "ServiceNow - admin"
-        description: "Project containing users ServiceNow playbooks for admin use"
-        organization: Default
-        state: present
-        scm_type: git
-        scm_url: https://github.com/cloin/instruqt-snow
-        controller_host: "https://localhost"
-        controller_username: admin
-        controller_password: ansible123!
-        validate_certs: false
-
-    - name: Post SNOW user create job template
-      awx.awx.job_template:
-        name: "0 - Create SNOW demo user"
-        job_type: "run"
-        organization: "Default"
-        inventory: "Demo Inventory"
-        project: "ServiceNow - admin"
-        playbook: "admin_project/create-snow-user.yml"
-        execution_environment: "ServiceNow EE"
-        ask_variables_on_launch: true
-        credentials:
-          - "ServiceNow"
-        state: "present"
-        controller_host: "https://localhost"
-        controller_username: admin
-        controller_password: ansible123!
-        validate_certs: false
-
-    - name: Launch SNOW user create/destroy job
-      awx.awx.job_launch:
-        job_template: "0 - Create SNOW demo user"
-        extra_vars:
-          cleanup: false
-        controller_host: https://localhost
-        controller_username: admin
-        controller_password: ansible123!
-        validate_certs: false
-
 EOF
 
 # chown above file
-sudo chown rhel:rhel /home/rhel/setup-controller.yml
+sudo chown rhel:rhel /tmp/setup-controller.yml
+sudo chown rhel:rhel /tmp/inventory
 
-# Run playbook 
-echo "execute setup-controller playbook"
-su - rhel -c 'ansible-playbook /home/rhel/setup-controller.yml'
+sleep 20
 
-EOF'
-
-# Enable linger so podman can function for user
-su - $USER -c 'loginctl enable-linger $USER'
-
-
-# Write ansible-navigator config
-su - $USER -c 'cat >/home/rhel/servicenow_project/ansible-navigator.yml <<EOL
----
-ansible-navigator:
-  execution-environment:
-    enabled: true
-    container-engine: podman
-    image: quay.io/acme_corp/servicenow-ee:latest
-    pull:
-      policy: never
-    environment-variables:
-      pass:
-        - SN_HOST
-        #- SN_USERNAME
-        #- SN_PASSWORD
-  playbook-artifact:
-    enable: true
-    save-as: "{playbook_dir}/artifacts/{playbook_name}-artifact-{time_stamp}.json"
-  logging:
-    append: true
-    file: 'artifacts/ansible-navigator.log'
-    level: warning
-  editor:
-    console: false
-EOL'
-
-# Set environment variables in bashrc for navigator
-su - $USER -c 'echo "export SN_HOST=https://ansible.service-now.com" >> /home/rhel/.bashrc'
-
-# Clean up old navigator
-su - $USER -c 'sudo dnf -y remove ansible-navigator'
-
-# Download checker playbooks
-wget -O /tmp/check-jt-run.yml https://raw.githubusercontent.com/cloin/snow-demo-setup/main/track_check_scripts/check-jt-run.yml
-wget -O /tmp/check-inventory-sync.yml https://raw.githubusercontent.com/cloin/snow-demo-setup/main/track_check_scripts/check-inventory-sync.yml
+ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup-controller.yml
