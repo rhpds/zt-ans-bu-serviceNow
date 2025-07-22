f796a53b6b0bde926270e99c0a258b482e21500f
Alex Walczyk <abwalczyk@gmail.com>
2025-07-22 15:11:56 -0400

    Running playbook

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -186,4 +186,4 @@ sudo chown rhel:rhel /tmp/inventory
 
 sleep 20
 
-#ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup-controller.yml
+ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup-controller.yml
