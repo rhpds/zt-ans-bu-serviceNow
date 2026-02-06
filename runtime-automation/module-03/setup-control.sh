47823802cfddce0fa9d84989cece32f0f626b770
Alex Walczyk <abwalczyk@gmail.com>
2026-02-06 12:48:58 -0500

    Testing

--- runtime-automation/module-03/setup-control.sh
+++ runtime-automation/module-03/setup-control.sh
@@ -39,14 +39,21 @@ tee /tmp/change-attach.yml << EOF
     servicenow.itsm.change_request:
       state: new
       type: standard
-      category: Software
-      short_description: "Reboot the webserver"
-      description: "Implementation plan: reboot affected servers"
+      category: software
+      short_description: "Reboot the webserver test"
+
+      implementation_plan: "Reboot affected servers"
       justification: "Reference to related problem or incident"
+      risk_impact_analysis: "Low risk change"
+      backout_plan: "Restart services if reboot fails"
+      test_plan: "Verify website is reachable"
+
       other:
         parent: "{{ item.number }}"
+
     loop: "{{ problem_list }}"
     register: change
+
 
   # - debug:
   #     msg: "A new change request has been created {{ change.record.number }}"
